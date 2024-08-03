a3278535559380b6f6b940ffe055312679cc5c85
Elphys Alvarez <50891953+ElphysAlvarez01@users.noreply.github.com>
2024-08-03 13:04:05 -0400

    Update Account.py
    formatting edits

--- Account.py
+++ Account.py
@@ -1,6 +1,7 @@
 """ Create a Account class with methods"""
 
 class Account:
+    
     """Creating an Account class with methods"""
     def __init__(self, balance, interest):
         self.balance = balance
@@ -8,10 +9,12 @@ class Account:
 
     # This method sets the balance of the account.
     def set_balance(self, balance):
+        
         """Sets the balance for the for the account"""
         self.balance = balance
 
     # The method sets the interest gained for the account.
     def set_interest(self, interest):
+        
         """Sets the interest gained for the the account"""
         self.interest = interest
